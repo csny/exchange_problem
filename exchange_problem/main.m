cb81f709d001f065f639039cc7936c1d19ec9144
csny <masaono_13@msn.com>
2014-08-06 13:11:53 +0900

    一部定数化

--- exchange_problem/main.m
+++ exchange_problem/main.m
@@ -8,12 +8,16 @@
 
 #import <Foundation/Foundation.h>
 
-int coinvalue[6] = {1,2,4,8,16,32}; // 硬貨の種類
-int remaindermemo[63]; // 支払い残額のメモ、再帰してforで使い回すので配列化
-int dp[63]; // 使った硬貨の組み合わせ記録、上書き上等
-int dpanswer[63]; // 使った硬貨の組み合わせの答え記録用
+
+const int kind = 6; // 硬貨の種類の数
+int coinvalue[kind] = {1,2,4,8,16,32}; // 硬貨の種類
+const int imax = 63; // 探索最深度
+int remaindermemo[imax]; // 支払い残額のメモ、再帰してforで使い回すので配列化
+int dp[imax]; // 使った硬貨の組み合わせ記録、上書き上等
+int dpanswer[imax]; // 使った硬貨の組み合わせの答え記録用
 int minnum=0; // 硬貨の最低使用枚数
 int hit=0; // 残額が丁度0になった組み合わせの数
+
 
 // 使った硬貨の組み合わせをコピー
 void cpdp(int k){
@@ -25,13 +29,13 @@ void cpdp(int k){
 // 残額63から硬貨の種類毎に引いてみる深さ優先探索
 void payment(int i,int remainder){
     
-    if (i > 63){return;} // 1x63が最大枚数
+    if (i > imax){return;} // 1x63が最大枚数なので、終了条件
     remaindermemo[i]=remainder;
     //NSLog(@"payrest:%d,i:%d",restmemo[i],i);
     
     
     if (remaindermemo[i] > 0){
-        for (int j = 5; j >= 0; j--){  // 硬貨の額の大きい方から試行
+        for (int j = kind-1; j >= 0; j--){  // 硬貨の額の大きい方から試行
             if(i==0 | coinvalue[j]<=dp[i-1]){ // 順列ではなく組み合わせにしたいので、前回より大きい硬貨は選ばない
                 dp[i] = coinvalue[j];
                 payment(i+1, remaindermemo[i] -= coinvalue[j]); // 残額から硬貨の額を引いて再帰呼出、深さ優先
